ac947fc0b819c26244e8f12ad25415f04db75f5e
Josh Walawender <jmwalawender@gmail.com>
2020-07-26 21:42:17 -1000

    Get rid of unused args

--- quicklook/scripts/script.py
+++ quicklook/scripts/script.py
@@ -24,20 +24,7 @@ def _parseArguments(in_args):
     parser.add_argument('-frames', nargs='*', type=str, help='input image file (full path, list ok)', default=None)
 
     # in this case, we are loading an entire directory, and ingesting all the files in that directory
-    parser.add_argument('-infiles', dest="infiles", help="Input files", nargs="*")
     parser.add_argument('-d', '--directory', dest="dirname", type=str, help="Input directory", nargs='?', default=None)
-    # after ingesting the files, do we want to continue monitoring the directory?
-    parser.add_argument('-m', '--monitor', dest="monitor", action='store_true', default=False)
-
-    # special arguments, ignore
-    parser.add_argument("-i", "--ingest_data_only", dest="ingest_data_only", action="store_true",
-                        help="Ingest data and terminate")
-    parser.add_argument("-w", "--wait_for_event", dest="wait_for_event", action="store_true", help="Wait for events")
-    parser.add_argument("-W", "--continue", dest="continuous", action="store_true",
-                        help="Continue processing, wait for ever")
-    parser.add_argument("-s", "--start_queue_manager_only", dest="queue_manager_only", action="store_true",
-                        help="Starts queue manager only, no processing",
-    )
 
     args = parser.parse_args(in_args[1:])
     return args
@@ -91,27 +78,18 @@ def main():
 
     framework.logger.info("Framework initialized")
 
-    # start queue manager only (useful for RPC)
-    if args.queue_manager_only:
-        # The queue manager runs for ever.
-        framework.logger.info("Starting queue manager only, no processing")
-        framework.start_queue_manager()
-
     # frames processing
-    elif args.frames:
+    if args.frames is not None:
         for frame in args.frames:
             # ingesting and triggering the default ingestion event specified in the configuration file
             framework.ingest_data(None, args.frames, False)
-            # manually triggering an event upon ingestion, if desired.
-            #arguments = Arguments(name=frame)
-            #framework.append_event('template', arguments)
 
     # ingest an entire directory, trigger "next_file" on each file, optionally continue to monitor if -m is specified
-    elif args.infiles is not None or args.dirname is not None:
+    elif args.dirname is not None:
         framework.logger.info(f'Ingesting files from {args.dirname}')
-        framework.ingest_data(args.dirname, args.infiles, args.monitor)
+        framework.ingest_data(args.dirname, None, True)
 
-    framework.start(args.queue_manager_only, args.ingest_data_only, args.wait_for_event, args.continuous)
+    framework.start(False, False, False, args.dirname is not None)
 
 
 if __name__ == "__main__":
