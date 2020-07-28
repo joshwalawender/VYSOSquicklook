df1b321b51f82e91d219be0b5930cf48c30a5c16
Josh Walawender <jmwalawender@gmail.com>
2020-07-28 10:25:04 -1000

    Tweak log level, remove regenerate plot from pipeline

--- vysosdrp/pipelines.py
+++ vysosdrp/pipelines.py
@@ -24,8 +24,8 @@ class QuickLookPipeline(BasePipeline):
         "make_source_mask": ("MakeSourceMask", "making_source_mask", "subtract_background"),
         "subtract_background": ("SubtractBackground", "subtracting_background", "extract"),
         "extract": ("ExtractStars", "extracting_stars", "record"),
-        "record": ("Record", "recording_results_in_mongo", "regenerate_plot"),
-        "regenerate_plot": ("RegeneratePlot", "generating_plot", None),
+        "record": ("Record", "recording_results_in_mongo", None),
+#         "regenerate_plot": ("RegeneratePlot", "generating_plot", None),
 #         "transfer_file": (),
     }
 
